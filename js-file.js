b78d04e2580e08c9646af771374ac767b8ef3f7c
apoorwaG <apoogiri@gmail.com>
2021-11-29 12:39:32 -0800

    changed flex layout to make container work with odd # squares

--- js-file.js
+++ js-file.js
@@ -1,7 +1,8 @@
 const gridContainer = document.querySelector(".gridContainer");
-const gridLength = 500;
-gridContainer.style.width = `${gridLength}px`;
-gridContainer.style.length = `${gridLength}px`;
+const gridLength = 700;
+// gridContainer.style.width = `${gridLength}px`;
+// gridContainer.style.length = `${gridLength}px`;
+// gridContainer.setAttribute('style', `width: ${gridLength}px; height: ${gridLength}px`);
 
 
 function setBoxHandW(box, numBoxesPerSide){
@@ -19,48 +20,125 @@ function getRandomRGB() {
 
 }
 
+function getRandomHSL() {
+    let h = Math.floor(Math.random() * 361);
+    let s = Math.floor(Math.random() * 101);
+    let l = Math.floor(Math.random() * 101);
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
+function darkenThisColor(oldColor) {
+    let colorValues = oldColor.slice(4, -1);
+    let colorArray = colorValues.split(", ");
+    let r = +colorArray[0];
+    let g = +colorArray[1];
+    let b = +colorArray[2];
+
+    let hslValues = rgbToHsl(r, g, b);
+    let l = hslValues[2] * 100;
+    let h = hslValues[0] * 360;
+    let s = hslValues[1] * 100;
+
+    if(l - 10 <= 0) l = 0;
+    else l -= 10;
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
+
+}
+
+// give box a color. darken the color if it's already colored
 function visitBox(event) {
     const box = event.target;
-    console.log(box.style.backgroundColor);
-    box.style.backgroundColor = getRandomRGB();
+    const oldColor = box.style.backgroundColor;
+    if(oldColor === "") box.style.backgroundColor = getRandomRGB();
+    else {
+        const darkenedColor = darkenThisColor(oldColor);
+        box.style.backgroundColor = darkenedColor;
+    }
     event.stopPropagation();    // stop bubbling. default (capture: false) means stop bubbling up
                                 // capture: true means stop bubbling down
 }
 
 function populateGrid(gridContainer, numBoxesPerSide){
-    numBoxesTotal = numBoxesPerSide**2;
-    for(let i = 0; i < numBoxesTotal; i++){
-        const box = document.createElement('div');
-        box.classList.add('box');
-        // box.setAttribute('style', 'width: 48.75px; height: 48.75px;');
-        setBoxHandW(box, numBoxesPerSide);
-        gridContainer.appendChild(box);
+    const numBoxesTotal = numBoxesPerSide**2;
+
+    // for(let i = 0; i < numBoxesTotal; i++){
+    //     const box = document.createElement('div');
+    //     box.classList.add('box');
+    //     box.setAttribute('style', `flex: 0 0 auto; height: ${700/numBoxesPerSide}px; width: ${700/numBoxesPerSide}px`);
+    //     box.addEventListener('mouseover', visitBox);
+    //     gridContainer.appendChild(box);
+    //     console.log(1);
+    // }
+
+    for(let row = 0; row < numBoxesPerSide; row++){
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('rowDiv');
+        rowContainer.setAttribute('style', `display: flex;`);
+        for(let column = 0; column < numBoxesPerSide; column++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.setAttribute('style', `flex: 1 1 auto; height: ${700/numBoxesPerSide}px; width: ${700/numBoxesPerSide}px`);
+            box.addEventListener('mouseover', visitBox);
+            rowContainer.appendChild(box);
+            console.log(1);
+        }
+        
+        gridContainer.appendChild(rowContainer);
     }
-    
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-        box.addEventListener('mouseover', visitBox);
-    });
+
 }
 
 function resetGrid(event) {
-    let numBoxesPerSide = +prompt("Enter number of squares you want per side (between 1 and 100). \nDefault: 16", "16");
+    let numBoxesPerSide = prompt("Enter number of squares you want per side (between 1 and 100). \nDefault: 16", "16");
+    if (numBoxesPerSide === null) return;
 
+    // first remove the boxes in each row
+    const rowContainers = document.querySelectorAll('.rowDiv');
+    rowContainers.forEach(row => {
+        const boxes = row.childNodes;
+        boxes.forEach(box => {
+            row.removeChild(box);
+        });
+    });
+    
+
+    // now remove the rows one by one
     const container = document.querySelector('.gridContainer');
-
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-        container.removeChild(box);
+    const rows = document.querySelectorAll('.rowDiv');
+    rows.forEach(row => {
+        container.removeChild(row)
     });
 
+    numBoxesPerSide = +numBoxesPerSide;
 
-
-    if(!numBoxesPerSide) {
+    if(!numBoxesPerSide || numBoxesPerSide > 64) {
         populateGrid(container, 16);
     }
-    populateGrid(container, numBoxesPerSide);
 
-    
+    populateGrid(container, numBoxesPerSide);
 }
 
 populateGrid(gridContainer, 16);
